0c5780e2c65f2f7561c9d9b8a8fcdc9be1001d24
Hudson88 <dmitryboz21@yandex.com>
2023-03-23 11:05:11 +0300

    calendar

--- src/js/import/form.js
+++ src/js/import/form.js
@@ -7,6 +7,28 @@ $(window).on('load', () => {
 		dropdownAutoWidth: true,
 	});
 
-	
+	const datePickerInput = document.getElementById('date-picker');
+
+	// Initialize the date picker
+	var flatpickrItem=flatpickr(datePickerInput, {
+	  enableTime: true,
+	  dateFormat: 'Y-m-d',
+	  onOpen: [
+			function(selectedDates, dateStr, instance){
+			  var container=$(instance.calendarContainer);
+			  if(container.find('.datepicker-btns').length<1){
+			   container.find('.flatpickr-innerContainer').after('<div class="datepicker-btns"><div class="datepicker-btn datepicker-btn--reject js-datepicker-btn-reject">отменить</div><div class="datepicker-btn datepicker-btn--accept js-datepicker-btn-accept">выбрать</div></div>');
+			  }
+			}
+		],
+	});
+
+	$('body').on('click','.js-datepicker-btn-reject, .js-datepicker-btn-accept', function(){
+	 flatpickrItem.close();
+	});
+
+
+
+
 });
 
