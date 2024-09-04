d3b7a176a7d8c8ac8f3f85f63ce99b3259138008
FrontMD <web@mockup.digital>
2024-09-04 18:27:51 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 @@include("./libs/jquery.min.js")
 @@include("./libs/select2.full.min.js")
 @@include("./libs/masonry.pkgd.js")
-//@@include("../../node_modules/@fancyapps/ui/dist/fancybox/fancybox.umd.js")
+//include("../../node_modules/@fancyapps/ui/dist/fancybox/fancybox.umd.js")
 @@include("./libs/jquery.fancybox.min.js")
 @@include("./libs/jquery.mask.min.js")
 @@include("./libs/tippy/popper.min.js")
